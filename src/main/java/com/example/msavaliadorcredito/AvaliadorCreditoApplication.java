f608af47dee5be5d57f8ad549a627116639deb15
luizf <luizfelipelf2@gmail.com>
2023-04-11 21:40:16 -0300

    Initial commit

---
+++ src/main/java/com/example/msavaliadorcredito/AvaliadorCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.example.msavaliadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorCreditoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/msavaliadorcredito/application/controller/AvaliadorCreditoController.java
@@ -0,0 +1,15 @@
+package com.example.msavaliadorcredito.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("avaliacoes-credito")
+public class AvaliadorCreditoController {
+
+    @GetMapping
+    public String status() {
+        return "Ok";
+    }
+}
